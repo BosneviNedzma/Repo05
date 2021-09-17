f139740aea9dd0c06928321c2e01e1095c082e83
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-17 15:17:43 +0200

    Concatenating Strings with the Plus Equals Operator

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,9 +1,9 @@
 function main() {
     // Only change code below this line
-    var myString = "This is the first string. " + "This is the second string.";
+    var myString = "Concatenation is the process of appending ";
+    myString += "one string to the end of another string."
     // Only change code above this line
     return myString;
 }
-
 console.log(main());
 module.exports = main;
