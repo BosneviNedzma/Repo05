40392556d1ef38a6021ff2f8c73cc060adffb489
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-17 17:49:17 +0200

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks ="I was walking the " + myNoun + ". It was rain so I " + myVerb + ". A " + myAdjective + " car came by and I " + myAdverb + " got away.";
+    // Only change code above this line
+    return wordBlanks;
+}
+console.log (main());
+module.exports = main;
