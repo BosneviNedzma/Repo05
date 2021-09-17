c4d3ea7bd16cad93735a7ec7eea9d62e66348444
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-17 15:37:58 +0200

    Constructing Strings with Variables

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    var myName = "Mujesira";
+    var myString = "My name is " + myName + " and I am well!";
+    // Only change code aobve this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
